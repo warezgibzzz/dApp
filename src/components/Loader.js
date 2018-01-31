0a6c8757b39ee0c6056d08ade479e0a5e307742a
Perfect Makanju <damiperfect@gmail.com>
2018-01-31 17:27:06 +0100

    Create new UI flow for test query

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -2,29 +2,13 @@ import React from 'react';
 
 import splash from '../animations/splash.gif';
 
-const fullScreenStyle = {
-  position: 'absolute',
-  top: 0,
-  left: 0,
-  right: 0,
-  bottom: 0,
-
-  zIndex: 1,
-
-  background: 'white',
-  opacity: 0.4,
-
-  alignItems: 'center',
-  justifyContent: 'center'
-};
-
 function Loader(props) {
   const style = {
     display: props.loading ? 'flex' : 'none'
   };
 
   return (
-    <div style={{ ...fullScreenStyle, ...style }}>
+    <div style={{ ...style }}>
       <img alt="Market Loader" src={splash} />
     </div>
   );
