610fd6c5b9784c8321f9069561b0d8f2917f5a75
Eswara Sai <eswar.vankayalapati@gmail.com>
2018-02-02 20:08:55 +0530

    Added link for documentation in navbar

--- src/components/NavItem.js
+++ src/components/NavItem.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { withRouter } from 'react-router';
-
-import Link from './Link.js';
 
 class NavItem extends Component {
   render () {
