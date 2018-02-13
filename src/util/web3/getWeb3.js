b60d0c9c5a08fe55c5fae46057ddd1f346c1e35a
Cryptomental <cryptomental.com@gmail.com>
2018-02-13 17:15:57 +0100

    getWeb3.js: Verify that MetaMask is unlocked.
    The check was extended to verify that MetaMask is available and if it is unlocked.
    
    Issue: #35

--- src/util/web3/getWeb3.js
+++ src/util/web3/getWeb3.js
@@ -17,8 +17,19 @@ let getWeb3 = new Promise(function(resolve, reject) {
     var web3 = window.web3;
 
     // Checking if Web3 has been injected by the browser (Mist/MetaMask)
-    if (typeof web3 !== 'undefined') {
-      // Use Mist/MetaMask's provider.
+    if (typeof web3 !== 'undefined' && web3.currentProvider && web3.currentProvider.isMetaMask) {
+      console.log("Mist/MetaMask's detected!");
+
+      if (web3.eth.accounts.length === 0) {
+        results = {
+          web3Instance: null
+        };
+
+        console.log("Please unlock MetaMask!");
+
+        resolve(store.dispatch(web3Initialized(results)));
+      }
+
       web3 = new Web3(web3.currentProvider);
 
       results = {
