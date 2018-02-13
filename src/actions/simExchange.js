b2c52d8dea52e98ca85801a4b267f1f2a5317469
Phil Elsasser <phil@marketprotocol.io>
2018-02-12 20:03:45 -0700

    some ugly code for @eswara ;(

--- src/actions/simExchange.js
+++ src/actions/simExchange.js
@@ -1,6 +1,10 @@
 import store from '../store';
 import abi from '../util/MarketContractABI';
-import { signMessage, getContractParams } from '../util/utils';
+import { getBids, getAsks} from '../util/utils';
+import OrderLib from '../build/contracts/OrderLib';
+
+const contract = require('truffle-contract');
+
 
 export function selectContract(contract) {
   return function(dispatch) {
@@ -12,19 +16,37 @@ export function tradeOrder(order) {
   const type = 'TRADE_ORDER';
   const web3 = store.getState().web3.web3Instance;
 
-  return function(dispatch) {
+  return async function(dispatch) {
     dispatch({ type: `${type}_PENDING` });
 
     if (web3 && typeof web3 !== 'undefined') {
       const contract_address = store.getState().simExchange.contract.key;
-      const contract = web3.eth.contract(abi).at(contract_address);
+      const marketContract = web3.eth.contract(abi).at(contract_address);
 
-      console.log(order);
+      const orderLib = contract(OrderLib);
+      orderLib.setProvider(web3.currentProvider);
+      let orderLibInstance = await orderLib.deployed();
 
-      // Get required params to execute trade order
-      getContractParams(contract_address);
+      // here are active bids and aks from our bot account - note the bot account still needs to
+      // deposit collateral in order for these orders to be executed against.
+      const activeBids = await getBids(web3, marketContract, orderLibInstance);
+      const activeAsks = await getAsks(web3, marketContract, orderLibInstance);
+      console.log(activeBids);
 
-      // contract.tradeOrder();
+      const orderToTrade = activeAsks[0];
+      await marketContract.tradeOrder(
+        orderToTrade.orderAddresses,
+        orderToTrade.unsignedOrderValues,
+        orderToTrade.orderQty,                  // qty is five
+        -1,          // let us fill a one lot
+        orderToTrade.v,  // v
+        orderToTrade.r,  // r
+        orderToTrade.s,  // s
+        {from: web3.eth.accounts[0]}
+      );
+
+      // NOTE: this is a very rough example of how this could all work.  Essentially the user selects an order object
+      // to trade against and calls trade order from their account.
 
       dispatch({ type: `${type}_FULFILLED`, payload: order });
     } else {
--- src/util/utils.js
+++ src/util/utils.js
@@ -1,3 +1,6 @@
+const BUY_SIGN = 1;
+const SELL_SIGN = -1;
+
 /**
  * Signs a message.
  *
@@ -12,7 +15,7 @@ export const signMessage = function (web3, address, message) {
   const s = `0x${signature.slice(66, 130)}`;
   let v = web3.toDecimal(`0x${signature.slice(130, 132)}`);
   if (v !== 27 && v !== 28) v += 27;
-  return [v,r,s];
+  return [v, r, s];
 };
 
 /**
@@ -43,7 +46,136 @@ export const calculateCollateral = function (priceFloor, priceCap, qtyMultiplier
   return maxLoss * Math.abs(qty) * qtyMultiplier;
 };
 
-// TODO : Get the required contract addresses and v,r,s
-export const getContractParams = function (address) {
-  // console.log(address);
+// TODO: move me to wherever I belong -clean up, add documentation, figure out how best to create order object in JS
+export const getBids = function (web3, marketContract, orderLib) {
+  // for now we will create orders around the contract mid price, eventually we should create orders
+  // that are around an price pulled from an active API that mimics the oracle solution
+  const contractMidPrice = (marketContract.PRICE_FLOOR.call().toNumber() +
+    marketContract.PRICE_CAP.call().toNumber()) / 2;
+  // we will need to fix this, the server will need to have an unlocked account created the order for the user to match
+  // here we are just using an account that we only have access to in the dev environment, but wont be able
+  // to use when we attempt to deploy to a test net (rinkeby)
+  return createNewOrders(
+    web3,
+    marketContract,
+    orderLib,
+    web3.eth.accounts[9],
+    contractMidPrice - BUY_SIGN, // subtract our sign so our market are not crossed.
+    SELL_SIGN,
+    5
+  );
 };
+
+
+export const getAsks = function (web3, marketContract, orderLib) {
+  // for now we will create orders around the contract mid price, eventually we should create orders
+  // that are around an price pulled from an active API that mimics the oracle solution
+  const contractMidPrice = (marketContract.PRICE_FLOOR.call().toNumber() +
+    marketContract.PRICE_CAP.call().toNumber()) / 2;
+  // we will need to fix this, the server will need to have an unlocked account created the order for the user to match
+  // here we are just using an account that we only have access to in the dev environment, but wont be able
+  // to use when we attempt to deploy to a test net (rinkeby)
+  return createNewOrders(
+    web3,
+    marketContract,
+    orderLib,
+    web3.eth.accounts[9],
+    contractMidPrice - SELL_SIGN, // subtract our sign so our market are not crossed.
+    SELL_SIGN,
+    5
+  );
+};
+
+
+const createNewOrders = async function (web3,
+                                        marketContract,
+                                        orderLib,
+                                        makerAccount,
+                                        startingPrice,
+                                        mktSign,
+                                        desiredOrderCount) {
+  if (desiredOrderCount <= 0)
+    return null;
+
+  startingPrice = Math.trunc(startingPrice);  //convert to integer
+  const orders = [];
+  const orderQty = 1 * mktSign;               // for now all orders have qty of 1 (+1 for bid, -1 for sell)
+  const expirationTimeStamp = Math.floor(Date.now() / 1000) + 86400;  // order expires in a day.
+  const takerAccount = null;
+  const feeRecipient = null;
+  const makerFee = 0;
+  const takerFee = 0;
+
+  for (let i = 0; i < desiredOrderCount; i++) {
+    const newOrderPrice = startingPrice - (i * mktSign);
+    const order = new Order(marketContract.address, makerAccount, takerAccount, feeRecipient, makerFee, takerFee,
+      newOrderPrice, expirationTimeStamp, 1, orderQty);
+    await order.getOrderHash(orderLib);
+    order.signOrder(web3, makerAccount);
+    orders.push(order);
+  }
+
+  return orders;
+};
+
+
+
+class Order {
+
+  constructor(contractAddress,
+              maker,
+              taker,
+              feeRecipient,
+              makerFee,
+              takerFee,
+              price,
+              expirationTimeStamp,
+              salt,
+              orderQty,
+  ) {
+    this.contractAddress = contractAddress;
+    this.maker = maker;
+    this.taker = taker;
+    this.feeRecipient = feeRecipient;
+    this.makerFee = makerFee;
+    this.takerFee = takerFee;
+    this.price = price;
+    this.expirationTimeStamp = expirationTimeStamp;
+    this.salt = salt;
+    this.orderQty = orderQty;
+    this.orderHash = null;
+    this.orderAddresses = [maker, taker, feeRecipient];
+    this.unsignedOrderValues = [makerFee, takerFee, price, expirationTimeStamp, salt];
+    this.v = null;
+    this.r = null;
+    this.s = null;
+    this.isSigned = false;
+  }
+
+  async getOrderHash(orderLib) {
+    if(this.orderHash != null)
+      return this.orderHash;
+
+    let orderHash = await orderLib.createOrderHash.call(
+      this.contractAddress,
+      this.orderAddresses,
+      this.unsignedOrderValues,
+      this.orderQty
+    );
+
+    this.orderHash = orderHash;
+    return this.orderHash;
+  }
+
+  signOrder(web3, makerAccount) {
+    if(this.isSigned)
+      return;
+    const signature = signMessage(web3, makerAccount, this.orderHash);
+    this.v = signature[0];
+    this.r = signature[1];
+    this.s = signature[2];
+    this.isSigned = true;
+  }
+
+}
+
