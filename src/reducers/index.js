d0343b9ee89eb191e44ea932bcb85853a6425e42
Cryptomental <cryptomental.com@gmail.com>
2018-03-03 14:12:22 +0100

    reducers: Add find reducer to reducers index.
    Issue: #10

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,6 +3,7 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import web3Reducer from '../util/web3/web3Reducer';
 
+import find from './find';
 import deploy from './deploy';
 import explorer from './explorer';
 import testQuery from './testQuery';
@@ -10,6 +11,7 @@ import testQuery from './testQuery';
 export default combineReducers({
   routing: routerReducer,
   web3: web3Reducer,
+  find: find,
   deploy: deploy,
   explorer: explorer,
   testQuery: testQuery,
