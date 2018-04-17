75d817da331789283a8fdce4e5d1d1ec4ea451ea
Cryptomental <cryptomental.com@gmail.com>
2018-04-17 23:29:22 +0200

    getWeb3.test.js: Refactor tests to use properly mocked web3.
    Issue: #111

--- test/util/web3/getWeb3.test.js
+++ test/util/web3/getWeb3.test.js
@@ -20,50 +20,46 @@ describe('getWeb3', () => {
     };
   });
 
-  // helper to create a mock provider
-  function getMockProvider() {
-    const mockProvider = new FakeProvider();
-    mockProvider.isMetaMask = true;
-    // mockProvider.host = 'ws://remotenode.com:8546';
-    return mockProvider;
+  function mockedWeb3(callbackError = null, coinbaseAddress = '0x123456', accounts = ['0x0000001']) {
+    const fakeProvider = new FakeProvider();
+    const web3 = new Web3(fakeProvider);
+    fakeProvider.injectResult(accounts);
+    web3.eth.getCoinbase = (callback) => { callback(callbackError, coinbaseAddress); };
+    web3.eth.getAccounts = (callback) => { callback(callbackError, accounts); };
+    web3.eth.getNetwork = (callback) => { callback(callbackError, '4447'); };
+    web3.currentProvider.isMetaMask = true;
+    return web3;
   }
 
   // helper to setup window parameters
-  function setupWindow({ provider, accounts }) {
+  function setupWindow(web3Instance) {
     Object.assign(mockWindow, {
-      web3: {
-        currentProvider: provider,
-        eth: { accounts },
-      },
+      web3: web3Instance,
     });
   }
 
   it('should dispatch with web3.version.network as Truffle Develop Network', async () => {
-    const truffleDevelopNetworkId = '4447';
     const expectedNetwork = 'truffle';
-    const mockProvider = getMockProvider();
-    mockProvider.injectResult(truffleDevelopNetworkId);
-    setupWindow({ provider: mockProvider, accounts: [{}] });
-    
+
     await getWeb3(mockWindow, showErrorMessageSpy, dispatchSpy);
 
     const dispatchCall = dispatchSpy.getCall(0);
     const actualNetwork = dispatchCall.args[0].payload.network;
     expect(actualNetwork).to.equals(expectedNetwork);
   });
-  
+
   it('should fallback to default web3 provider if web3 provider does not exist', async () => {
     const defaultProviderHost = 'http://127.0.0.1:9545';
     await getWeb3(mockWindow, showErrorMessageSpy, dispatchSpy);
-    
+
     const dispatchCall = dispatchSpy.getCall(0);
     const web3Instance = dispatchCall.args[0].payload.web3Instance;
     expect(web3Instance.currentProvider.host).to.equals(defaultProviderHost);
   });
 
   it('should have proper results', async () => {
-    const mockProvider = getMockProvider();
-    setupWindow({ provider: mockProvider, accounts: [{}] }); // accounts must not be empty
+    const web3 = mockedWeb3();
+    setupWindow(web3); // accounts must not be empty
 
     await getWeb3(mockWindow, showErrorMessageSpy, dispatchSpy);
 
@@ -74,20 +70,20 @@ describe('getWeb3', () => {
     expect(payload).to.have.property('network');
   });
 
-  it('should dispatch web3.currentProvider as web3Instance', async () => {
-    const mockProvider = getMockProvider();
-    setupWindow({ provider: mockProvider, accounts: [{}] }); // accounts must not be empty
+  it('should dispatch web3 as web3Instance', async () => {
+    const web3 = mockedWeb3();
+    setupWindow(web3);
 
     await getWeb3(mockWindow, showErrorMessageSpy, dispatchSpy);
 
     const dispatchCall = dispatchSpy.getCall(0);
     const web3Instance = dispatchCall.args[0].payload.web3Instance;
-    expect(web3Instance.currentProvider).to.equals(mockProvider);
+    expect(web3Instance).to.have.property('currentProvider');
   });
 
   it('should show Error message when default provider is locked', async () => {
-    const mockProvider = getMockProvider();
-    setupWindow({ provider: mockProvider, accounts: [] }); // account is empty when locked
+    const web3 = mockedWeb3(null, '0x123456', []);
+    setupWindow(web3); // account is empty when locked
 
     await getWeb3(mockWindow, showErrorMessageSpy, dispatchSpy);
 
