e1dd86492e2a9fb0452d1b086c2a286a39ff20bc
Phil Elsasser <phil@marketprotocol.io>
2018-05-23 16:36:27 -0600

    update deploy with new solidity 0.0.28

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -52,71 +52,78 @@ module.exports = function(deployer, network) {
       // deploy MKT token
       const marketTokenToLockForTrading = 0; // for testing purposes, require no lock
       const marketTokenAmountForContractCreation = 0; //for testing purposes require no balance
-      deployer.deploy(
-        MarketToken,
-        marketTokenToLockForTrading,
-        marketTokenAmountForContractCreation
-      );
+      return deployer
+        .deploy(
+          MarketToken,
+          marketTokenToLockForTrading,
+          marketTokenAmountForContractCreation
+        )
+        .then(function() {
+          // deploy collateral token
+          deployer.deploy(CollateralToken, 'FakeDollars', 'FUSD', 1e9, 18);
 
-      // deploy collateral token
-      deployer.deploy(CollateralToken, 'FakeDollars', 'FUSD', 1e9, 18);
+          const daysToExpiration = 28;
+          const expirationDate = new Date();
+          expirationDate.setDate(expirationDate.getDate() + daysToExpiration);
+          //const expirationString = expirationDate.toISOString().substring(0, 10);
+          //const marketContractExpiration = Math.floor(Date.now() / 1000) + 86400 * daysToExpiration; // expires in 28 days
 
-      const daysToExpiration = 28;
-      const expirationDate = new Date();
-      expirationDate.setDate(expirationDate.getDate() + daysToExpiration);
-      //const expirationString = expirationDate.toISOString().substring(0, 10);
-      //const marketContractExpiration = Math.floor(Date.now() / 1000) + 86400 * daysToExpiration; // expires in 28 days
-
-      // deploy and set up main factory to create MARKET Protocol smart contracts.
-      return MarketContractRegistry.deployed().then(function(
-        marketContractRegistry
-      ) {
-        return deployer
-          .deploy(MarketContractFactory, marketContractRegistry.address, {
-            gas: 6400000,
-            from: web3.eth.accounts[0]
-          })
-          .then(function(factory) {
-            // white list the factory
-            return marketContractRegistry
-              .addFactoryAddress(factory.address)
-              .then(function() {
-                // deploy a single contract for testing purposes.
-                const gasLimit = 4000000; // gas limit for contract deployment
-                let quickExpirationTimeStamp =
-                  Math.floor(Date.now() / 1000) + 60 * 60; // expires in an hour
-                return factory
-                  .deployMarketContractOraclize(
-                    'ETHUSD_' + new Date().toISOString().substring(0, 10),
-                    MarketToken.address,
-                    CollateralToken.address,
-                    [50000, 150000, 2, 1e18, quickExpirationTimeStamp],
-                    'URL',
-                    'json(https://api.kraken.com/0/public/Ticker?pair=ETHUSD).result.XETHZUSD.c.0',
-                    { gas: gasLimit, from: web3.eth.accounts[0] }
-                  )
-                  .then(function(marketContractDeployResults) {
-                    const marketContractDeployedAddress =
-                      marketContractDeployResults.logs[0].args.contractAddress;
-                    return deployer
-                      .deploy(
-                        MarketCollateralPool,
-                        marketContractDeployedAddress,
-                        { gas: 2200000 }
+          // deploy and set up main factory to create MARKET Protocol smart contracts.
+          return MarketContractRegistry.deployed().then(function(
+            marketContractRegistry
+          ) {
+            return deployer
+              .deploy(
+                MarketContractFactory,
+                marketContractRegistry.address,
+                MarketToken.address,
+                {
+                  gas: 6500000,
+                  from: web3.eth.accounts[0]
+                }
+              )
+              .then(function(factory) {
+                // white list the factory
+                return marketContractRegistry
+                  .addFactoryAddress(factory.address)
+                  .then(function() {
+                    // deploy a single contract for testing purposes.
+                    const gasLimit = 4000000; // gas limit for contract deployment
+                    let quickExpirationTimeStamp =
+                      Math.floor(Date.now() / 1000) + 60 * 60; // expires in an hour
+                    return factory
+                      .deployMarketContractOraclize(
+                        'ETHUSD_' + new Date().toISOString().substring(0, 10),
+                        CollateralToken.address,
+                        [50000, 150000, 2, 1e18, quickExpirationTimeStamp],
+                        'URL',
+                        'json(https://api.kraken.com/0/public/Ticker?pair=ETHUSD).result.XETHZUSD.c.0',
+                        { gas: gasLimit, from: web3.eth.accounts[0] }
                       )
-                      .then(function(marketCollateralPool) {
-                        return MarketContractOraclize.at(
-                          marketContractDeployedAddress
-                        ).then(function(marketContractInstance) {
-                          return marketContractInstance.setCollateralPoolContractAddress(
-                            marketCollateralPool.address
-                          );
-                        });
+                      .then(function(marketContractDeployResults) {
+                        const marketContractDeployedAddress =
+                          marketContractDeployResults.logs[0].args
+                            .contractAddress;
+                        return deployer
+                          .deploy(
+                            MarketCollateralPool,
+                            marketContractDeployedAddress,
+                            { gas: 2200000 }
+                          )
+                          .then(function(marketCollateralPool) {
+                            return MarketContractOraclize.at(
+                              marketContractDeployedAddress
+                            ).then(function(marketContractInstance) {
+                              return marketContractInstance.setCollateralPoolContractAddress(
+                                marketCollateralPool.address
+                              );
+                            });
+                          });
                       });
                   });
               });
           });
-      });
+        });
     });
   }
 };
