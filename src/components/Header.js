fafe8135fc92c53ba221bd0a234a798978270b91
Jonathan Sanders <jonsandersss@gmail.com>
2018-06-15 08:49:42 -0400

    bugfix for issue #191 - navbar state (#197)

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,47 +8,52 @@ import '../less/Header.less';
 const SubMenu = Menu.SubMenu;
 const MenuItem = Menu.Item;
 
+const subMenuItems = [
+  {Name: 'Quick Deploy', to: '/contract/deploy?mode=quick'}, 
+  {Name: 'Simplified Deploy', to: '/contract/deploy?mode=simplified'},
+  {Name: 'Guided Deploy', to: '/contract/deploy?mode=guided'}
+];
+
+const mainMenuItems = [
+  {Name: 'Explore Contracts', to: '/contract/explorer'}, 
+  {Name: 'Find Contracts', to: '/contract/find'},
+  {Name: 'Test Query', to: '/test'},
+  {Name: 'Documentation', to: 'http://docs.marketprotocol.io', target: '_blank'}
+];
+
 class Header extends Component {
+  state = {
+    selected: ''
+  };
+
+  onClick = (e) => {
+    this.setState({ selected: e.key});
+  }
+
   render() {
     return (
       <Affix>
         <header id="header" className="clearfix">
           <div className="ant-row">
             <div className="ant-col-xs-24 ant-col-sm-24 ant-col-md-6 ant-col-lg-5 ant-col-xl-5 ant-col-xxl-4">
-              <Link to="/" id="logo">
+              <Link to="/" id="logo" onClick={this.onClick}>
                 <img src={logo} alt="Market Protocol Logo" />
               </Link>
             </div>
             <div className="ant-col-xs-0 ant-col-sm-0 ant-col-md-18 ant-col-lg-19 ant-col-xl-19 ant-col-xxl-20">
-              <Menu mode="horizontal" className="menu-site" id="nav">
+              <Menu mode="horizontal" className="menu-site" id="nav" selectedKeys={[this.state.selected]}>
                 <SubMenu title="Deploy Contracts">
-                  <MenuItem>
-                    <Link to="/contract/deploy?mode=quick">Quick Deploy</Link>
-                  </MenuItem>
-                  <MenuItem>
-                    <Link to="/contract/deploy?mode=simplified">
-                      Simplified Deploy
-                    </Link>
-                  </MenuItem>
-                  <MenuItem>
-                    <Link to="/contract/deploy?mode=guided">Guided Deploy</Link>
-                  </MenuItem>
+                  {subMenuItems.map((item) =>
+                    <MenuItem key={item.to} onClick={this.onClick}>
+                      <Link to={item.to}>{item.Name}</Link>
+                    </MenuItem>
+                  )}
                 </SubMenu>
-                <MenuItem>
-                  <Link to="/contract/explorer">Explore Contracts</Link>
-                </MenuItem>
-                <MenuItem>
-                  <Link to="/contract/find">Find Contracts</Link>
-                </MenuItem>
-                <MenuItem>
-                  <Link to="/test">Test Query</Link>
-                </MenuItem>
-                {/*<MenuItem><Link to="/exchange">Sim Exchange</Link></MenuItem>*/}
-                <MenuItem>
-                  <Link to="http://docs.marketprotocol.io" target="_blank">
-                    Documentation
-                  </Link>
-                </MenuItem>
+                {mainMenuItems.map((item) =>
+                  <MenuItem key={item.to} onClick={this.onClick}>
+                    <Link to={item.to} target={item.target}>{item.Name}</Link>
+                  </MenuItem>
+                )}
               </Menu>
             </div>
           </div>
